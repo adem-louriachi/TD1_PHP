d1041f290689e08b4a11f95bcf1cadb1f4fea3ef
LOURIACHI Adem <adem.louriachi@etu.univ-amu.fr>
2019-09-13 10:47:14 +0200

    4eme test

--- TD1_PHP.php
+++ TD1_PHP.php
@@ -18,6 +18,12 @@
 <body>
 
 <?php
+
+$var1 = 6;
+$var2 = 1.3;
+$var3 = "Variable 3";
+
+
 echo '<strong> Voici mon premier programme </strong><br/>' . "\n";
 echo 'C\'est  pas mal non ? <br/>';
 ?>
