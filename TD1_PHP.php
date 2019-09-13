7a7a34eb7c9213622a9659b5c1437c40f8bd0a3a
LOURIACHI Adem <adem.louriachi@etu.univ-amu.fr>
2019-09-13 10:28:14 +0200

    troisième test

--- TD1_PHP.php
+++ TD1_PHP.php
@@ -1,1 +1,25 @@
+<!DOCTYPE HTML>
+<html lang=fr>
+
+<head>
+    <!-- Balise Meta -->
+    <meta charset="UTF-8"/>
+    <meta name="author" content="Adem Louriachi" />
+    <meta name="description" content="Site pour le projet du 1er semestre DUT Informatique, professeur : Olivier Gérard"/>
+    <meta name="keywords" content="Adem, Adem_Swallow, projet, écriture, informatique, hirondelle, plume">
+    <title>La Plume de l'Hirondelle - Accueil</title>
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="csssite/index.css"/>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" sizes="32x32" href="imagessite/favicon-32x32.png">
+
+</head>
+
+<body>
+
 <?php
+echo '<strong> Voici mon premier programme </strong><br/>' . "\n";
+echo 'C\'est  pas mal non ? <br/>';
+?>
+</body>
+</html>
