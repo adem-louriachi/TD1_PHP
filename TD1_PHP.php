9d3bf621994dee5b74c627a72876583f8dd96f93
LOURIACHI Adem <adem.louriachi@etu.univ-amu.fr>
2019-09-13 11:02:58 +0200

    Decouverte du PHP

--- TD1_PHP.php
+++ TD1_PHP.php
@@ -23,6 +23,10 @@ $var1 = 6;
 $var2 = 1.3;
 $var3 = "Variable 3";
 
+echo "$var1 + $var2";
+echo $var1 + $var2;
+echo $var1 + $var3;
+
 
 echo '<strong> Voici mon premier programme </strong><br/>' . "\n";
 echo 'C\'est  pas mal non ? <br/>';
