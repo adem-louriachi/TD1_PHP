c61c139bcd8f9a729f185cb47850c0bec7fd9af0
LOURIACHI Adem <l15017560@A-F002073.salsa.univ-amu.fr>
2019-09-16 15:00:55 +0200

    Facto amélio

--- calculator.php
+++ calculator.php
@@ -13,16 +13,22 @@ $operateurs = '*+-/';
 ?>
 
 <h1>Testons les calculs !</h1>
-<form action="calcul.php" method="GET">
+<form action="calcul.php" method="post">
     <p>
         <label for="Op1">Op1</label><input id="op1" type="text" name="op1" value=" "/>
         <label for="Op2">Op2</label><input id="op2" type="text" name="op2" value=" "/><br/>
-
-        <input type="submit" name="action" value="+/>
-        <input type="submit" name="action" value="*"/>
-        <input type="submit" name="action" value="-"/>
-        <input type="submit" name="action" value="/"/>
-        <input type="submit" name="action" value="Calculez"/>
+        <?php
+        for($cpt = 0 ; $cpt <= 3 ; ++$cpt)
+        {
+            echo '<input ';
+            if($cpt == 0)
+            {
+                echo 'checked="checked" ';
+            }
+            echo 'type="radio" name="op" value="' . $operateurs[$cpt] . '"/>' . $operateurs[$cpt] . ' <br/>' . "\n";
+        }
+        ?>
+        <input type="submit" name="action" value="Calculez_ "/>
     </p>
 </form>
 
